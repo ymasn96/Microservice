2f1d0369b8cbcc083732db26669019e049d52c96
Yasir Siddig <yastheman@Yasirs-MacBook-Pro-2.local>
2022-06-06 16:04:03 +0100

    Removed @SpringBootTest annotation in Test Class

--- src/test/java/com/tsi/yasir/siddig/myMicroService/MyMicroServiceApplicationTests.java
+++ src/test/java/com/tsi/yasir/siddig/myMicroService/MyMicroServiceApplicationTests.java
@@ -3,7 +3,7 @@ package com.tsi.yasir.siddig.myMicroService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class MyMicroServiceApplicationTests {
 
 	@Test
