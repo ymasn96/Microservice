1f4b2124807d20444232231edfc6d6d3cfc1b891
Yasir Siddig <ymansiddig@gmail.com>
2022-06-09 12:52:46 +0100

    Added Film class and interface

---
+++ src/main/java/com/tsi/yasir/siddig/myMicroService/Film.java
@@ -0,0 +1,148 @@
+package com.tsi.yasir.siddig.myMicroService;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="film")
+public class Film {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int film_id;
+    private String title;
+    private String description;
+    private int release_year;
+    private String language_id;
+    private String original_language_id;
+    private int rental_duration;
+    private float rental_rate;
+    private int length;
+    private float replacement_cost;
+    private int rating;
+    private String special_features;
+
+    public Film(String title,
+                String description,
+                int release_year,
+                String language_id,
+                String original_language_id,
+                int rental_duration,
+                float rental_rate,
+                int length,
+                float replacement_cost,
+                int rating,
+                String special_features)
+    {
+        this.title = title;
+        this.description = description;
+        this.release_year = release_year;
+        this.language_id = language_id;
+        this.original_language_id = original_language_id;
+        this.rental_duration = rental_duration;
+        this.rental_rate = rental_rate;
+        this.length = length;
+        this.replacement_cost = replacement_cost;
+        this.rating = rating;
+        this.special_features = special_features;
+    }
+
+    public Film() {
+    }
+
+    public int getFilm_id() {
+        return film_id;
+    }
+
+    public void setFilm_id(int film_id) {
+        this.film_id = film_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getRelease_year() {
+        return release_year;
+    }
+
+    public void setRelease_year(int release_year) {
+        this.release_year = release_year;
+    }
+
+    public String getLanguage_id() {
+        return language_id;
+    }
+
+    public void setLanguage_id(String language_id) {
+        this.language_id = language_id;
+    }
+
+    public String getOriginal_language_id() {
+        return original_language_id;
+    }
+
+    public void setOriginal_language_id(String original_language_id) {
+        this.original_language_id = original_language_id;
+    }
+
+    public int getRental_duration() {
+        return rental_duration;
+    }
+
+    public void setRental_duration(int rental_duration) {
+        this.rental_duration = rental_duration;
+    }
+
+    public float getRental_rate() {
+        return rental_rate;
+    }
+
+    public void setRental_rate(float rental_rate) {
+        this.rental_rate = rental_rate;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public float getReplacement_cost() {
+        return replacement_cost;
+    }
+
+    public void setReplacement_cost(float replacement_cost) {
+        this.replacement_cost = replacement_cost;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getSpecial_features() {
+        return special_features;
+    }
+
+    public void setSpecial_features(String special_features) {
+        this.special_features = special_features;
+    }
+}
---
+++ src/main/java/com/tsi/yasir/siddig/myMicroService/FilmRepository.java
@@ -0,0 +1,6 @@
+package com.tsi.yasir.siddig.myMicroService;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface FilmRepository extends CrudRepository<Film, Integer> {
+}
