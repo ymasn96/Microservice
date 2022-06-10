be908d222dbf41fb32f092bd8707e3dacc78f69b
Yasir Siddig <ymansiddig@gmail.com>
2022-06-10 15:06:20 +0100

    Added test string for demo purposes

--- src/main/java/com/tsi/yasir/siddig/myMicroService/MyMicroServiceApplication.java
+++ src/main/java/com/tsi/yasir/siddig/myMicroService/MyMicroServiceApplication.java
@@ -16,6 +16,7 @@ public class MyMicroServiceApplication {
 
 	@Autowired
 	private ActorRepository actorRepo;
+	private String testString;
 	@Autowired
 	private CategoryRepository categoryRepo;
 	@Autowired
