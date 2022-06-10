8f101fc23fea1ac0797984844a270de0fddea9f1
Yasir Siddig <ymansiddig@gmail.com>
2022-06-10 13:43:14 +0100

    Modified Film Class by removing unused attributes

--- src/main/java/com/tsi/yasir/siddig/myMicroService/Film.java
+++ src/main/java/com/tsi/yasir/siddig/myMicroService/Film.java
@@ -16,36 +16,27 @@ public class Film {
     private int release_year;
     private String language_id;
     private String original_language_id;
-    private int rental_duration;
-    private float rental_rate;
     private int length;
-    private float replacement_cost;
+
     private int rating;
-    private String special_features;
+
 
     public Film (String title,
                 String description,
                 int release_year,
                 String language_id,
                 String original_language_id,
-                int rental_duration,
-                float rental_rate,
                 int length,
-                float replacement_cost,
-                int rating,
-                String special_features)
+                int rating
+   )
     {
         this.title = title;
         this.description = description;
         this.release_year = release_year;
         this.language_id = language_id;
         this.original_language_id = original_language_id;
-        this.rental_duration = rental_duration;
-        this.rental_rate = rental_rate;
         this.length = length;
-        this.replacement_cost = replacement_cost;
         this.rating = rating;
-        this.special_features = special_features;
     }
 
     public Film() {
@@ -99,36 +90,12 @@ public class Film {
         this.original_language_id = original_language_id;
     }
 
-    public int getRental_duration() {
-        return rental_duration;
-    }
-
-    public void setRental_duration(int rental_duration) {
-        this.rental_duration = rental_duration;
-    }
-
-    public float getRental_rate() {
-        return rental_rate;
-    }
-
-    public void setRental_rate(float rental_rate) {
-        this.rental_rate = rental_rate;
-    }
-
     public int getLength() {
         return length;
     }
 
     public void setLength(int length) {
         this.length = length;
-    }
-
-    public float getReplacement_cost() {
-        return replacement_cost;
-    }
-
-    public void setReplacement_cost(float replacement_cost) {
-        this.replacement_cost = replacement_cost;
     }
 
     public int getRating() {
@@ -139,11 +106,4 @@ public class Film {
         this.rating = rating;
     }
 
-    public String getSpecial_features() {
-        return special_features;
-    }
-
-    public void setSpecial_features(String special_features) {
-        this.special_features = special_features;
-    }
 }
